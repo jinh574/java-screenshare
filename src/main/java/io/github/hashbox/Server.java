0c8b1432d98cdae58ae543790b7e91f1a598d31d
한지승 <jinh574@naver.com>
2017-05-17 01:41:15 +0900

    Add source code

---
+++ src/main/java/io/github/hashbox/Server.java
@@ -0,0 +1,55 @@
+package io.github.hashbox;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Vector;
+
+
+public class Server implements Runnable {
+	final int tcp_port = 9000;
+	ServerGUI servGUI;
+	Vector list;
+	Socket clntSock;
+	
+	public Server() {
+		servGUI = new ServerGUI(this);
+		new Thread(servGUI).start();
+	}
+	public void run() {
+		list = new Vector();
+		try {
+			ServerSocket server = new ServerSocket(tcp_port);
+			System.out.println("[SYSTEM]서버가 시작되었습니다.");
+			while(true) {
+				clntSock = server.accept();
+				ServerThread st = new ServerThread(servGUI, clntSock, this);
+				addThread(st); //벡터에 스레드를 담는다.
+				st.start();
+			}
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	public void addThread(ServerThread st) {
+		  list.add(st);//벡터에 쓰레드를 담는다.
+	}//addThread
+	
+	public void removeThread(ServerThread st) {
+		  list.remove(st);//벡터에서 해당 쓰레드를 없앤다.
+	}//removeThread
+	
+	public void broadcast(String str) throws IOException {
+		for (int i = 0; i < list.size(); i++) {
+			ServerThread st = (ServerThread) list.elementAt(i);
+			st.send(str);//각각의 쓰레드마다 채팅 내용 전송한다.
+		}
+	}
+	
+	public boolean doShare(int i) throws IOException {
+		ServerThread st = (ServerThread) this.list.elementAt(i);
+		st.isShare = true;
+		
+		return true;
+	}
+}
